c20d3726644fff9da2ab326570cfd62c832511bf
Jennifer Pazos <jmp521@gmail.com>
2020-01-28 20:07:52 -0600

    initial commit

---
+++ lib/CLI/Mushroom.rb
@@ -0,0 +1,8 @@
+require "CLI/Mushroom/version"
+
+module CLI
+  module Mushroom
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/CLI/Mushroom/version.rb
@@ -0,0 +1,5 @@
+module CLI
+  module Mushroom
+    VERSION = "0.1.0"
+  end
+end
